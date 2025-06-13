d37315b850714c4fc539d14b0a02423ad7d8be96
Felipe Mammoli <fmammoli@gmail.com>
2025-06-13 18:48:34 -0300

    a minimal signalling server

---
+++ src/server.ts
@@ -0,0 +1,46 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "https://gaiasenses-web.vercel.app", // Replace with your Next.js app URL
+    methods: ["GET", "POST"],
+    credentials: false,
+  },
+  connectionStateRecovery: {},
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.id);
+
+  socket.on("offer", (sdp) => {
+    socket.broadcast.emit("getOffer", sdp);
+    console.log("offer: " + socket.id);
+  });
+
+  socket.on("answer", (sdp) => {
+    socket.broadcast.emit("getAnswer", sdp);
+    console.log("answer: " + socket.id);
+  });
+
+  socket.on("candidate", (candidate) => {
+    socket.broadcast.emit("getCandidate", candidate);
+    console.log("candidate: " + socket.id);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+app.get("/", (_req, res) => {
+  res.send("WebRTC Signaling Server is running!");
+});
+
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
